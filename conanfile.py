a0aaeaa4e7f4c6c9302888360bde645ff24bdf9f
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-04-26 14:28:47 -0500

    Recipe draft

---
+++ conanfile.py
@@ -0,0 +1,29 @@
+from conans import ConanFile, CMake, tools
+
+
+class ExtracmakemodulesConan(ConanFile):
+    name = "extra-cmake-modules"
+    version = "5.57.0"
+    license = "MIT"
+    author = "Alexis Lopez Zubieta contact@azubieta.net"
+    url = "https://github.com/appimage-conan-community/conan-extra-cmake-modules"
+    description = "Extra modules and scripts for CMake"
+    topics = ("cmake", "ecm", "kde")
+    settings = "os", "compiler", "build_type", "arch"
+    # options = {"shared": [True, False]}
+    # default_options = "shared=False"
+    build_requires = "cmake_installer/3.10.0@conan/stable"
+
+    def source(self):
+        self.run("git clone git://anongit.kde.org/extra-cmake-modules.git --depth=1 --branch=v5.57.0")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="extra-cmake-modules")
+        cmake.build()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.builddirs = ["share/ECM/cmake"]
+
+
---
+++ test_package/conanfile.py
@@ -0,0 +1,19 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+
+class ExtracmakemodulesTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake_paths"
+    build_requires = "cmake_installer/3.10.0@conan/stable"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["CMAKE_PROJECT_PackageTest_INCLUDE"] = self.build_folder + "/conan_paths.cmake"
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not tools.cross_building(self.settings):
+            self.run(".%sexample" % os.sep)
